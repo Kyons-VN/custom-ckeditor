3d5b5f703e94cd078174218ba1b18c153f86cc3a
mibi <binhhm2009@gmail.com>
2022-12-07 09:22:10 +0700

    Add responsive image feature

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -2,37 +2,40 @@
  * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
-import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import Image from '@ckeditor/ckeditor5-image/src/image.js';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
-import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
-import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageBlockEditing from '@ckeditor/ckeditor5-image/src/image/imageblockediting';
+import ImageInlineEditing from '@ckeditor/ckeditor5-image/src/image/imageinlineediting';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
 import ListProperties from '@ckeditor/ckeditor5-list/src/listproperties';
-import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat';
 import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters';
 import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
 import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter';
 import MathType from '@wiris/mathtype-ckeditor5';
 import CustomColorPlugin from './customcolorplugin';
 
@@ -96,6 +99,9 @@ Editor.builtinPlugins = [
   ImageStyle,
   ImageToolbar,
   ImageUpload,
+  ImageResize,
+  ImageBlockEditing,
+  ImageInlineEditing,
   Indent,
   Italic,
   Underline,
@@ -150,7 +156,45 @@ Editor.defaultConfig = {
   },
   language: 'en',
   image: {
-    toolbar: ['imageTextAlternative', 'imageStyle:inline', 'imageStyle:block', 'imageStyle:side'],
+    toolbar: [
+      'toggleImageCaption',
+      'imageStyle:block',
+      'imageStyle:inline',
+      'imageStyle:alignLeft',
+      'imageStyle:alignRight',
+      'imageStyle:side',
+      'imageStyle:alignBlockLeft',
+      'imageStyle:alignCenter',
+      'imageStyle:alignBlockRight',
+      'resizeImage',
+    ],
+    resizeUnit: '%',
+    resizeOptions: [
+      {
+        name: 'resizeImage:original',
+        value: null,
+        label: 'Original size',
+        // Note: add the "icon" property if you're configuring a standalone button.
+      },
+      {
+        name: 'resizeImage:100',
+        value: '50',
+        label: 'Small size',
+        // Note: add the "icon" property if you're configuring a standalone button.
+      },
+      {
+        name: 'resizeImage:600',
+        value: '70',
+        label: 'Medium size',
+        // Note: add the "icon" property if you're configuring a standalone button.
+      },
+      {
+        name: 'resizeImage:1024',
+        value: '100',
+        label: 'Large size',
+        // Note: add the "icon" property if you're configuring a standalone button.
+      },
+    ],
   },
   table: {
     contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties'],
